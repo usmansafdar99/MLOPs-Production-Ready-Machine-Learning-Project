1c6c5adc96f480f0cf29c3de0314f6dcb78371b1
usmansafdar99 <usmanlodhra99@gmail.com>
2026-05-12 16:05:44 +0500

    demo.py file added utils.py added

---
+++ demo.py
@@ -0,0 +1,9 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+logging.info("This is an info message")
+
+try:
+    a = 2/0
+except Exception as e:
+    raise USvisaException(e, sys)
